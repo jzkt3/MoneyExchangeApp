20e239dafb17553a8800b1ba1f1cfca720ea4942
jzkt3 <jayzee00723@gmail.com>
2015-03-04 23:17:29 -0600

    1

---
+++ app/src/main/java/jzkt3/umkc/edu/testtestetestsetestsetes/Keys.java
@@ -0,0 +1,15 @@
+package jzkt3.umkc.edu.testtestetestsetestsetes;
+
+/**
+ * Created by jzz on 3/4/2015.
+ */
+public interface Keys {
+    public interface EndpointExchangeRates{
+        public static final String KEY_DISCLAIMER ="disclaimer";
+        public static final String KEY_LICENSE = "license";
+        public static final String KEY_TIMESTAMP = "timestamp";
+        public static final String KEY_BASE = "base";
+        public static final String KEY_RATES = "rates";
+    }
+
+}
---
+++ app/src/main/java/jzkt3/umkc/edu/testtestetestsetestsetes/UrlEndpoints.java
@@ -0,0 +1,12 @@
+package jzkt3.umkc.edu.testtestetestsetestsetes;
+
+/**
+ * Created by jzz on 3/4/2015.
+ */
+public class UrlEndpoints {
+
+    public static final String URL_EXCHANGE_RATES = "http://openexchangerates.org/api/latest.json";
+    public static final String URL_CHAR_QUESTION = "?";
+    public static final String URL_PARAM_API_ID = "app_id=";
+
+}
